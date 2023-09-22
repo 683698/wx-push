dddef076a79cc53a2066a1e3a395b56cc5001d2a
683698 <145673260+683698@users.noreply.github.com>
2023-09-22 12:56:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxdb8cf4d0cbaa3f48",
+  appsecret: "159fa483b6f769935f12c51e7b915931",
+  template_id: "A_bReJoIosOFxQw5As6sWRbyPVqUBwr433HrtxJ1JRw",
+  touser: "ofGI16cr1Y5-NObhROedpEbg7GcY",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  key: "cde30c7e81ed4452893c861a8592a903",
+  city: "洛阳",
+  first_date: "2023-03-05",
 };
